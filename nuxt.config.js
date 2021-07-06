ba81722f09d69aef19e1404680b6c6211bed7d4c
Bruno Gonzales <holabrunogonzales@gmail.com>
2021-07-06 03:00:08 -0500

    deployment to gh pages config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+	router: {
+		base: "/superbien"
+	},
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'superbien',
